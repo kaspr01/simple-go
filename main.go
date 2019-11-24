5e03412328b437ce56e53909f8cf3e05c6806518
kaspr01 <49912071+kaspr01@users.noreply.github.com>
2019-11-24 19:37:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,38 +1,5 @@
 package main
-
-import (
-	"net/http"
-	"encoding/json"
-	"fmt"
-)
-
+import "fmt"
 func main() {
-	server := &http.Server{
-		Addr:":8090",
-	}
-
-	http.HandleFunc("/", hello)
-
-	fmt.Println("Server started on port 8090")
-	server.ListenAndServe()
-}
-
-func hello(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	payload := struct {
-		Status  string  `json:"status"`
-		Message string  `json:"message"`
-	}{
-		Status:"Success",
-		Message:"Hello world!",
-	}
-
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(response)
+    fmt.Println("Hello World!")
 }
